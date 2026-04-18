47a009d63b8830a362d023fb9bcd3883edef97c5
Nikitha <writetonikithakn@gmail.com>
2026-04-18 12:10:59 +0530

    TEST-101: added comment

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -11,6 +11,7 @@ export class ActivityComponent implements OnInit {
   listData = [];
   constructor(private service :ProjService) { }
 
+  // added comment
   ngOnInit() {
       this.service.getImages().subscribe( (resp:any) => {
        for(let i =0; i< 25 ; i++){
